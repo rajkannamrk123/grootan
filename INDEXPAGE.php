278f3ff192719a10ee329a565cfad09d2e4120fd
rajkannamrk123 <59719580+rajkannamrk123@users.noreply.github.com>
2021-06-14 11:48:01 +0530

    Add files via upload

---
+++ INDEXPAGE.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 
+	<title>INSERT CSV FILE</title>
+</head>
+<body>
+   <form action="fileupload.php" method="POST" enctype="mulipart/form-data">
+   	<input type="file" name="GROOT" />
+   	<button type="submit" value="submit">UPLOAD CSV FILE</button>
+ </form>     
+</body>
+</html>
